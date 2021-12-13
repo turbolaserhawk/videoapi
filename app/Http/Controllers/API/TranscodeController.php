dcd82b70789de4a0f1fa1e99c03b308710e560ec
qra6666 <qra6666@gmail.com>
2021-12-13 17:28:10 +0500

    multiple watermarks

--- app/Http/Controllers/API/TranscodeController.php
+++ app/Http/Controllers/API/TranscodeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use FFMpeg;
 
 class TranscodeController extends Controller
 {
@@ -13,7 +12,7 @@ class TranscodeController extends Controller
         foreach ($request->data as $detail){
             $ffmpeg = "ffmpeg";
             $filter = '-vf "';
-            $out = time().".mp4";
+            $out_name = time().".mp4";
             $input = public_path($detail['video']);
             if ($detail['allow_cut'] == "yes"){
                 $cut_from = $detail['cut']['from'];
@@ -41,42 +40,48 @@ class TranscodeController extends Controller
                 $filter .= "scale=$width:$height";
             }
             $filter .= '"';
-            $pre_vid = public_path("pre/".$out);
+            $pre_vid = public_path("pre/".$out_name);
             exec("$ffmpeg -y -r $fps -i $input $cut $filter $pre_vid 2> log.txt");
             //watermarks
-            if ($detail['primary_watermark'] == "yes"){
-                $pw_vid = public_path("pw/".$out);
-                $pw_path = public_path($detail['pw']['path']);
-                $pw_top = $detail['pw']['top'];
-                $pw_left = $detail['pw']['left'];
-                exec("$ffmpeg -y -i $pre_vid -i $pw_path -filter_complex \"[1][0]scale2ref=w='iw*5/100':h='ih*5/100'[wm][vid];[vid][wm]overlay=$pw_left:$pw_top'\" $pw_vid 2> pw.txt");
-            }else{
-                $pw_vid = $pre_vid;
+            $watermark_path = '';
+            $overlay = '';
+            $total_watermarks = count($detail['iw']);
+            foreach ($detail['iw'] as $key => $watermark){
+                $watermark_path .=  " -i ".public_path($watermark['path']);
+                $vid = 'vid'.$key;
+                $wm = 'wm'.$key;
+                $w_h = $watermark['h'];
+                $w_w = $watermark['w'];
+                $w_x = $watermark['x'];
+                $w_y = $watermark['y'];
+                $water = $key+1;
+                if ($watermark['between'] == "yes"){
+                    $b_from = $watermark['between_from'];
+                    $b_to = $watermark['between_to'];
+                    $between = ":enable='between(t,$b_from,$b_to)'";
+                }else{
+                    $between = '';
+                }
+                if ($key == 0){
+                    $in = 0;
+                    $out = 'out'.$water;
+                }else{
+                    $in = 'out'.$key;
+                    $out = 'out'.($water+1);
+                }
+                if ($key + 1 == $total_watermarks){
+                    $terminator = '';
+                }else{
+                    $terminator = "[$out];";
+                }
+                $overlay .= "[$water][$in]scale2ref=w='iw*$w_w/100':h='ih*$w_h/100'[$wm][$vid];[$vid][$wm]overlay=$w_x:$w_y$between $terminator";
             }
-            if ($detail['random_watermark'] == "yes"){
-                $rw_vid = public_path("rw/".$out);
-                $rw_path = public_path($detail['rw']['path']);
-                $x = "if(eq(mod(n\,30)\,0)\,sin(random(1))*W\,x)";
-                $y = "if(eq(mod(n\,30)\,0)\,sin(random(1))*H\,y)";
-                $rw_from = $detail['rw']['from'];
-                $rw_to = $detail['rw']['to'];
-                $rw_between = "enable='between(t,$rw_from,$rw_to)'";
-                exec("ffmpeg -y -i $pw_vid -i $rw_path -filter_complex \"[1][0]scale2ref=w='iw*5/100':h='ih*5/100'[wm][vid];[vid][wm]overlay=$x:$y:$rw_between'\" $rw_vid 2> rw.txt");
-            }else{
-                $rw_vid = $pw_vid;
-            }
-            if ($detail['full_watermark'] == "yes"){
-                $fw_vid = public_path("fw/".$out);
-                $fw_path = public_path($detail['fw']['path']);
-                $fw_from = $detail['fw']['from'];
-                $fw_to = $detail['fw']['to'];
-                $fw_between = "enable='between(t,$fw_from,$fw_to)'";
-                exec("ffmpeg -y -i $rw_vid -i $fw_path -filter_complex \"[1][0]scale2ref=w='iw*100/100':h='ih*100/100'[wm][vid];[vid][wm]overlay=0:0:$fw_between\" $fw_vid 2> fw.txt");
-
-            }else{
-                $fw_vid = $rw_vid;
-            }
-            return response()->json($fw_vid);
+            $output = public_path("output/".$out_name);
+            $cmd = "ffmpeg -y -i $pre_vid $watermark_path -filter_complex \"$overlay\" $output 2> final.txt";
+            exec($cmd);
+            $size = array('width'=>$detail['resize']['width'],'height'=>$detail['resize']['height'],'fps'=>$detail['fps']);
+            $res = array('format'=>'mp4','size'=>$size);
+            return response()->json(['output'=>$res]);
         }
         //exec("ffmpeg -y -i $pre -i logo.jpg -filter_complex \"[1][0]scale2ref=w='iw*100/100':h='ih*100/100'[wm][vid];[vid][wm]overlay=0:0:enable='between(t,2,4)'\" full.mp4");
 
